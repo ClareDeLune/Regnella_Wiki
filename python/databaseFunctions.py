1bf8e3c5bd2825c775bfb462fd233d2a9683b1d8
Takumi <Example@template.com>
2024-12-03 14:14:35 +0000

    ATTEMPTED to add Database functionality, but FAILED!!

--- python/databaseFunctions.py
+++ python/databaseFunctions.py
@@ -1,14 +1,14 @@
 ###Set-Up
 from flask import Flask, render_template, g
-import sqlite3
+import sqlite3, os
 
 app = Flask(__name__)
 
 
 def openDatabase():
     def create_database():
-        db_location = 'database/regnellaDB.db'
-
+        base = os.path.abspath(os.path.dirname(__name__))
+        db_location = base + "\database\RegnellaDB.db"
         database = getattr(g, 'database', None)
         if database is None:
             database = sqlite3.connect(db_location)
@@ -21,22 +21,17 @@ def closeDatabase(exception):
         database.close()
 
 
-def accessChar(id):
+def accessChar(name):
     db = openDatabase()
-    try:
-        dbc = db.cursor()
+    dbc = db.cursor()
 
-        data = []
-        sql = "SELECT * FROM characters WHERE characters.FirstName = (%id)", id
-        for column in db.cursor().execute(sql):
+    data = []
+    sql = "SELECT * FROM characters WHERE characters.FirstName = (%name)", name
+    for column in db.cursor().execute(sql):
             data.append(str(column))
 
-        character = {"firstName": data[0], "lastName": data[1], "age": data[2], "race": data[3], "family": data[4], "type": data[5], "location": data[6]}
-        return render_template('PageHTML/individCharPage.html', character=character)
-    except:
-        print("An error has occured while trying to access the database.<br>Please try again later.")
-        return render_template('PageHTML/individCharPage.html')
-
+    character = {"firstName": data[0], "lastName": data[1], "age": data[2], "race": data[3], "family": data[4], "type": data[5], "location": data[6]}
+    return render_template('PageHTML/individCharPage.html', character=character)
 
 
 
